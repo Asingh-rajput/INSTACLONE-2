b10bf5f38ef423b9c0328b547d4b244b41c94673
Asingh-rajput <asingh507068@gmail.com>
2023-02-27 22:28:34 +0530

    file added

---
+++ src/components/data.js
@@ -0,0 +1,24 @@
+const post=[
+    {name:"Siva",
+    location:"Bengaluru",
+    likes:64,
+    description: "Kick start your career",
+    PostImage: "https://th.bing.com/th/id/OIP.PYipJ_hSncugM2SwnZitvgHaEK?pid=ImgDet&rs=1",
+    date: new Date(),
+    },
+    {name:"Neeraj",
+    location:"Pune",
+    likes:30,
+    description: "Sample Description",
+    PostImage: "https://th.bing.com/th/id/OIP.YAXlTjvtEKchdMVc5laZhwHaE8?pid=ImgDet&rs=1",
+    date: new Date(),
+    },
+    {name:"Rahul",
+    location:"Hyderabad",
+    likes:30,
+    description: "Sample Description for Post",
+    PostImage: "https://th.bing.com/th/id/OIP.MZk__RXOEgaif8GCngydjwHaEo?w=284&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+    date: new Date(),
+    }
+    ]
+    export default post;
